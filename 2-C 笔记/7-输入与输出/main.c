3842a5c7c6b98102f91bfb85a406e16ff6e31297
chen_chang_zhao <2047638475@qq.com>
2023-05-26 13:34:10 +0800

    feat: 🎸 输入与输出

--- 2-C 笔记/7-输入与输出/main.c
+++ 2-C 笔记/7-输入与输出/main.c
@@ -1,10 +1,12 @@
-#include<stdio.h>
-#include<ctype.h>
-#include<locale.h>
-int main(){
-  int year,mouth,day;
+#include <stdio.h>
+#include <ctype.h>
+#include <locale.h>
+int main()
+{
+  int year, mouth, day;
+  scanf("%d % d %d", &year, &mouth, &day);
   printf("请输入年月日，之间用空格隔开");
-  printf("%d % d %d",year,mouth,day);
-  
+  printf("%d年 %d 月%d 日", year, mouth, day);
+
   return 0;
 }
