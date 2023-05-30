9db63b828bf4b62c912fa79b9a28b95a81883dad
chen_chang_zhao <2047638475@qq.com>
2023-05-30 23:38:50 +0800

    modified:   "2-C \347\254\224\350\256\260/9-\345\206\205\345\255\230\345\210\206\345\214\272/a.exe" 	modified:   "2-C \347\254\224\350\256\260/9-\345\206\205\345\255\230\345\210\206\345\214\272/main.c" 	modified:   "2-C \347\254\224\350\256\260/9-\345\206\205\345\255\230\345\210\206\345\214\272/\345\206\205\345\255\230\345\210\206\345\214\272.md"

--- 2-C 笔记/9-内存分区/main.c
+++ 2-C 笔记/9-内存分区/main.c
@@ -1,18 +1,17 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-// 宏函数
-#define ADD(x, y) ((x) + (y))
 
 struct Person
 {
-    char *name; // 字符串
-    int age;
+    char name; // 字符串
+    int age;   // 这个内存存放的地址原则一 距离得是基数的整数倍（它本身占用大小与对齐模数的比较 取小的）这里取4，它又是第二个属性，只能从4开始
+    // 最后还要整体二次偏移 取最大的基数 的整数倍
 };
 
 int main()
 {
-    int a = 10, b = 100;
-    printf("a+b=%d", ADD(a, b) * 20);
+
+    printf("内存大小：%d\n", sizeof(struct Person));
     return 0;
 }
